c36818d908912276cb33f2e66d16ecb40e99a65f
Edward S. Vinyard <ed@tzahk.com>
2020-08-22 13:17:27 -0500

    fix compilation and style errors; Spring Boot Magic is stil broken so it does not run

--- entrypoint/src/main/java/com/example/entrypoint/Main.java
+++ entrypoint/src/main/java/com/example/entrypoint/Main.java
@@ -2,9 +2,7 @@ package com.example.entrypoint;
 
 import com.example.springbootweb.SpringbootwebApplication;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(SpringbootwebApplication.class, args);
