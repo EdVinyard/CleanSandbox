989105560971c35fe3253605ca8acb80cb16f3d6
Edward S. Vinyard <ed@tzahk.com>
2020-08-22 12:21:52 -0500

    add Spring Boot Web module

--- entrypoint/src/main/java/com/example/entrypoint/Main.java
+++ entrypoint/src/main/java/com/example/entrypoint/Main.java
@@ -1,11 +1,12 @@
 package com.example.entrypoint;
 
+import com.example.springbootweb.SpringbootwebApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) {
-        SpringApplication.run(Main.class, args);
+        SpringApplication.run(SpringbootwebApplication.class, args);
     }
 }
---
+++ springbootweb/src/test/java/com/example/springbootweb/SpringbootwebApplicationTests.java
@@ -0,0 +1,21 @@
+package com.example.springbootweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class SpringbootwebApplicationTests {
+
+    /*
+
+        TODO: I don't think this is appropriate here, because this module
+        doesn't have access to implementations of all the interfaces defined in
+        the Domain.
+
+    */
+
+    @Test
+	void contextLoads() {
+	}
+}
