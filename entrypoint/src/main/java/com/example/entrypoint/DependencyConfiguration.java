870362a01339e50a8c536cce635d5d6984c3f567
Edward S. Vinyard <ed@tzahk.com>
2020-08-22 09:31:38 -0500

    add CheckStyle, starting with Google-ish settings

--- entrypoint/src/main/java/com/example/entrypoint/DependencyConfiguration.java
+++ entrypoint/src/main/java/com/example/entrypoint/DependencyConfiguration.java
@@ -1,5 +1,5 @@
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class DependencyConfiguration {
